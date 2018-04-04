9633992c83b25df3e9851bff68efaee78f8afd05
Doug Enos <moolvie@gmail.com>
2018-04-04 09:13:07 -0400

    in the process of refactoring the code and avoiding duplicate records

--- AddRecord.php
+++ AddRecord.php
@@ -9,56 +9,48 @@ $SongLength = $_POST['SongLength'];
 $DBConnect = mysqli_connect("localhost","root","crumplebatverifytree", $DBName);
 if ($DBConnect === FALSE)
 	echo "<p>Connect error: " . mysqli_error() . "</p>\n";
-else {
-        // Build Query to add artist name to Artist table
-	$ADDartist = "INSERT INTO Artist" . " (ArtistID, ArtistName)" .
-		 " VALUES (NULL, '" . $ArtistName. "');";
 
-	// Add artist name to Artist table
-	$DBConnect->query($ADDartist);
-	
-	// Build query to grab the newly created ArtistID 
-	$ANumber = "Select ArtistID from Artist where ArtistName = '" . $ArtistName . "'";
-	
-	// run query and store result
-	$result = $DBConnect->query($ANumber);
-	
-	// convert result to an associatve array
-	$ArtistNumber = $result->fetch_assoc();
-	
+else {
+	$checkForArtist = 'Select ArtistID from Artist where ArtistName = "'. $ArtistName . '"';
+	$result = mysqli_query($DBConnect,$checkForArtist);
+	$rowcount = mysqli_num_rows($result);
+	if ($rowcount == 0){
+        	// Build Query to add artist name to Artist table
+		$ADDartist = "INSERT INTO Artist" . " (ArtistID, ArtistName)" .
+			 " VALUES (NULL, '" . $ArtistName. "');";
+
+		// Add artist name to Artist table
+		$DBConnect->query($ADDartist);
+		$result = mysqli_insert_id($DBConnect);	
+	}else{
+		$ArtistNumber = 'SELECT ArtistID FROM Artist WHERE ArtistName = "' . 
+			$ArtistName . '"';
+		$DBConnect->query($ArtistNumber);
+		$result = mysqli_query($DBConnect,$ArtistNumber);
+		$row = mysqli_fetch_row($result);
+		$ArtistNumber = $row[0];
+	}
+
 	// Build query for Song Table
 	$ADDSongTitle ='INSERT INTO Song' .' (SongID, ArtistID, SongTitle, SongLength)' .
-		' VALUES (NULL, ' . $ArtistNumber['ArtistID'] . ', "' . $SongTitle .
-		 '", "' . $SongLength . '")';
+		' VALUES (NULL, ' . $ArtistNumber . ', "' . $SongTitle .
+		 '", "' . $SongLength . '");';
+
 	// run query 
 	$DBConnect->query($ADDSongTitle);
 
+	$SongNumber = mysqli_insert_id($DBConnect);
+
 	// build query for Album
 	$ADDalbum = "INSERT INTO Album" . " (AlbumID, AlbumName, ArtistID)" .
-		" VALUES (NULL, '" . $AlbumTitle . "', " . $ArtistNumber['ArtistID'] . ")";
+		" VALUES (NULL, '" . $AlbumTitle . "', " . $ArtistNumber . ");";
 	$DBConnect->query($ADDalbum);
 
-	// Build query to grab newly created AlbumID
-	$Album_ID = "SELECT AlbumID FROM Album WHERE AlbumName = '" .  $AlbumTitle . "'";
-	
-	// run query and store result
-	$result = $DBConnect->query($Album_ID);
-	
-	// convert result to an associatve array
-	$AlbumNumber = $result->fetch_assoc();
-
-	// build query to grab SongID from Songs
-	$Song_ID = 'SELECT SongID FROM Song WHERE SongTitle = "' . $SongTitle . '"';
-
-	// run query and store result
-	$result = $DBConnect->query($Song_ID);
-
-	// convert result to an associative array
-	$SongNumber = $result->fetch_assoc();
+	$AlbumNumber = mysqli_insert_id($DBConnect);
 
         // build query to store AlbumID and SongID in AlbumSong
 	$ADDalbumInfo = "INSERT INTO AlbumSong" . " (AlbumID, SongID)" . " VALUES (" .
-		$AlbumNumber['AlbumID'] . ", " . $SongNumber['SongID'] . ")";
+		$AlbumNumber . ", " . $SongNumber . ")";
 
 	// run the query
 	$DBConnect->query($ADDalbumInfo);
@@ -67,3 +59,28 @@ else {
 	mysqli_close($DBConnect);
 }
 ?>
+<html>
+<style>
+label {
+	display: inline-block;
+	width: 100px;
+}
+</style>
+<BODY>
+<FORM METHOD="POST" ACTION="AddRecord.php"
+<p>
+<LABEL>Artist Name</LABEL>
+<INPUT TYPE="text" name="ArtistName" required></p>
+<p>
+<LABEL>Song Title</Label>
+<INPUT type="text" name="SongTitle" required></p>
+<p>
+<LABEL>Album Title</LABEL>
+<INPUT type="text" name="AlbumTitle"></p>
+<p>
+<LABEL>Song Duration</LABEL>
+<INPUT type="text" name="SongLength"></p>
+<label></label>
+<INPUT type="submit" name="Submit">
+</FORM>
+</BODY>
